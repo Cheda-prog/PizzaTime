57162567a41f3173146135aa1d7d47acf5f57362
Cheda-prog <chedaalex6@gmail.com>
2026-01-13 00:29:05 -0800

    typesafe

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,22 @@
 import Color from "@/src/constants/Colors";
 import { Image, StyleSheet, Text, View } from "react-native";
+import { Product } from "../components/types";
 
-const ProductlistItem = ({ product }) => {
+export const DefaultImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductlistItemProp = {
+  product: Product;
+};
+
+const ProductlistItem = ({ product }: ProductlistItemProp) => {
   console.log(product);
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.Image} />
+      <Image
+        source={{ uri: product.image || DefaultImage }}
+        style={styles.Image}
+      />
       <Text style={styles.title}> {product.name}</Text>
       <Text style={styles.price}>{product.price}</Text>
     </View>
